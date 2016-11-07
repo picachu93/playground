a2aded69c6a19c6450e1a07dbf94d6119186ad22
aman7461@gmail.com <aman7461@gmail.com>
2016-11-07 18:21:55 +0200

    coplete needs correction

--- playground.c
+++ playground.c
@@ -35,8 +35,7 @@ int main (int argc, char** argv) {
   }
 
 /**
-  *It counts for each grid-column the number of trees up to the cureent 
-  *grid-row
+  * pSum is the prefix currSums of the original matrix
   */
   int pSum[N][N];
   int tSum;
@@ -66,6 +65,59 @@ int main (int argc, char** argv) {
   }
 
 /*---------------------------------*/
+/**
+  * (i)For each pair of rows let be p,q it is counted the sub-
+  * arrays with currSum k.AscurrSumed that  p < q.
+  * (ii)It is  established a two point algorithm which uses 
+  * two pointers the start and the end starting in column
+  * zero.
+  * (iii)Find the currSum of implicit values
+  * pSum(q,j) - pSum(p,j) between the rows p,q.
+  * While currSum of values is less than k the start is advanced 
+  * and while currSum of values is more than k end is advanced.
+  * If currSum is k then increase the result(number of k-currSum sub-
+  * submatrices) and advance end and start until currSum is changed.
+  * Add to result the product of pointer advances.
+  * 
+  */
+  int start;
+  int end;
+  int currSum;
+  int result = 0;
+  for ( int p = 0; p < N; p++ ) {
+
+    for ( int q = 0; q < N; q++ ) {
+
+      start = 0;
+      end = 0;
+      currSum = pSum[q][0] - pSum[p][0];
+      while ( end < N ) {
+       
+        if ( currSum == K ) {
+
+          result++;
+
+        } else if ( currSum < K ) {
+
+          end++;
+          if ( end < N) {
+
+            currSum = currSum + ( pSum[q][end] - pSum[p][end] );
+
+          }
+
+        } else {
+
+          currSum = currSum - ( pSum[q][start] - pSum[p][start] );
+          start++;
+
+        }
+ 
+      }
+
+    }
+  }
+
   fclose(fp);
   return EXIT_STATUS;
 
